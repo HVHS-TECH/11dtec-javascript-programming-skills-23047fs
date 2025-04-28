95af308ba1b13b35c769028638694cabcc7346ef
23047fs <23047fs@hvhs.school.nz>
2025-04-28 21:46:53 +0000

    Apr 28, 2025, 9:46 PM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -6,6 +6,7 @@ console.log("Running t05_alerts_prompts.js");
 //Variables
 let userNAME = "null"
 let userAGE = "null"
+let pocketMONEY = "null"
 
 
 //Arrays
@@ -17,7 +18,7 @@ Main code
 alert("Welcome to this Program.");
 userNAME = prompt("What is your name?")
 userAGE = prompt("Welcome " + userNAME + " what is your age")
-
+pocketMONEY = prompt("How much pocket money do you have?")
 
 /********************************
 Functions
