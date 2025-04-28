89ec6fa80d507800e1b073101618a2b12c92ffaa
23047fs <23047fs@hvhs.school.nz>
2025-04-28 21:55:53 +0000

    Apr 28, 2025, 9:55 PM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -21,6 +21,8 @@ alert("Welcome to this Program.");
 userNAME = prompt("What is your name?");
 userAGE = prompt("Welcome " + userNAME + " what is your age");
 pocketMONEY = prompt("How much pocket money do you have?");
+userAge = Number(userAGE);
+pocketMONEY = Number(pocketMONEY);
 alert("You were born in " + (currentDATE - userAGE));
 alert("In ten years you will be " + (userAGE + 10));
 alert("You have $" + pocketMONEY);
