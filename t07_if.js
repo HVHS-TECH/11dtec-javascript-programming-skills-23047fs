6a735b2ea3a0c21081e075b871961c71c732f041
23047fs <23047fs@hvhs.school.nz>
2025-04-29 23:51:22 +0000

    Apr 29, 2025, 11:51 PM

--- t07_if.js
+++ t07_if.js
@@ -1,26 +1,38 @@
 /********************************
-Name of task: If
+Name of task: For Loops
 Author: Finley
-Date: 29/04/35
+Date: 30/04/35
 ********************************/
 console.log("Running t07_if.js");
 
 //Variables
-let userNAME = 0;
-
+let userNAME = 0
+let userAGE = 0
+let pocketMONEY = 0
+let currentDATE = 2025;
+let userCHOCOLATE = 0
 
 //Arrays
-let userNUMBER = [0, 0, 0, 0, 0];
-let userNUMEBRcount = 0
+let chocolate = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!"]
 
 /********************************
 Main code
 ********************************/
 alert("Welcome to this Program.");
 userNAME = prompt("What is your name?");
-for (count = 0; count < 5; count++){
-    userNUMBER[userNUMEBRcount] = prompt("Welcome " + userNAME + " choose any number");
-    userNUMEBRcount += 1
+userAGE = prompt("Welcome " + userNAME + " what is your age");
+pocketMONEY = prompt("How much pocket money do you have?");
+userCHOCOLATE = prompt("How much do you love chocolate from 0-3")
+userAGE = Number(userAGE);
+pocketMONEY = Number(pocketMONEY);
+alert("You were born in " + (currentDATE - userAGE));
+alert("In ten years you will be " + (10 + userAGE));
+alert("You have $" + pocketMONEY);
+alert("Half your pocket money would be $" + (pocketMONEY / 2));
+if (pocketMONEY >= 4){
+    alert("You think " + chocolate[userCHOCOLATE] + " and you can afford a bar of chocolate :)")
+} else {
+    alert("You think " + chocolate[userCHOCOLATE] + " and you can't afford a bar of chocolate :(")
 }
 
 
--- t08_for_loop.js
+++ t08_for_loop.js
@@ -1,38 +1,26 @@
 /********************************
-Name of task: For Loops
+Name of task: If
 Author: Finley
-Date: 30/04/35
+Date: 29/04/35
 ********************************/
 console.log("Running t07_if.js");
 
 //Variables
-let userNAME = 0
-let userAGE = 0
-let pocketMONEY = 0
-let currentDATE = 2025;
-let userCHOCOLATE = 0
+let userNAME = 0;
+
 
 //Arrays
-let chocolate = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!"]
+let userNUMBER = [0, 0, 0, 0, 0];
+let userNUMEBRcount = 0
 
 /********************************
 Main code
 ********************************/
 alert("Welcome to this Program.");
 userNAME = prompt("What is your name?");
-userAGE = prompt("Welcome " + userNAME + " what is your age");
-pocketMONEY = prompt("How much pocket money do you have?");
-userCHOCOLATE = prompt("How much do you love chocolate from 0-3")
-userAGE = Number(userAGE);
-pocketMONEY = Number(pocketMONEY);
-alert("You were born in " + (currentDATE - userAGE));
-alert("In ten years you will be " + (10 + userAGE));
-alert("You have $" + pocketMONEY);
-alert("Half your pocket money would be $" + (pocketMONEY / 2));
-if (pocketMONEY >= 4){
-    alert("You think " + chocolate[userCHOCOLATE] + " and you can afford a bar of chocolate :)")
-} else {
-    alert("You think " + chocolate[userCHOCOLATE] + " and you can't afford a bar of chocolate :(")
+for (count = 0; count < 5; count++){
+    userNUMBER[userNUMEBRcount] = prompt("Welcome " + userNAME + " choose any number");
+    userNUMEBRcount += 1
 }
 
 
