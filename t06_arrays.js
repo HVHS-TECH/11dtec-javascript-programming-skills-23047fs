ce1abe74e2d9bcebb45cdc2f6f7afea888ec0b4c
23047fs <23047fs@hvhs.school.nz>
2025-04-29 04:22:43 +0000

    Apr 29, 2025, 4:22 AM

--- t06_arrays.js
+++ t06_arrays.js
@@ -10,9 +10,10 @@ let userNAME = 0
 let userAGE = 0
 let pocketMONEY = 0
 let currentDATE = 2025;
+let userCHOCOLATE = 0
 
 //Arrays
-
+let chocolate = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!"]
 
 /********************************
 Main code
@@ -21,12 +22,14 @@ alert("Welcome to this Program.");
 userNAME = prompt("What is your name?");
 userAGE = prompt("Welcome " + userNAME + " what is your age");
 pocketMONEY = prompt("How much pocket money do you have?");
+userCHOCOLATE = prompt("How much do you love chocolate from 0-3")
 userAGE = Number(userAGE);
 pocketMONEY = Number(pocketMONEY);
 alert("You were born in " + (currentDATE - userAGE));
 alert("In ten years you will be " + (10 + userAGE));
 alert("You have $" + pocketMONEY);
 alert("Half your pocket money would be $" + (pocketMONEY / 2));
+alert(chocolate[userCHOCOLATE]);
 
 
 /********************************
