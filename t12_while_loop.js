2071ba7d6bfafe9df90a65889cc2811f495b71a3
23047fs <23047fs@hvhs.school.nz>
2025-05-14 00:12:13 +0000

    May 14, 2025, 12:12 AM

---
+++ t12_while_loop.js
@@ -0,0 +1,35 @@
+/********************************
+Name of task: While loops
+Author: Finley
+Date: 14/05/35
+********************************/
+console.log("Running while_loop.js");
+
+//Variables
+let isTRUE = true;
+let is = true;
+let loopCONTINUE;
+//Arrays
+
+
+/********************************
+Main code
+********************************/
+loopCONTINUE = prompt("Do you want this loop to continue, y or n");
+while (loopCONTINUE != "n") {
+    loopCONTINUE = prompt("Do you want this loop to continue, y or n");
+}
+alert("Sad :(");
+while (isTRUE == true){
+    if (is == true){
+        is = prompt("true or false");
+        is = Boolean(is)
+    } else {
+        isTRUE = false;
+    }
+}
+
+
+/********************************
+Functions
+********************************/
